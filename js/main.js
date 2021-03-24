5fd134032e2a20bc1323820193d15030638c9d9b
amoroabd <amoroabd@gmail.com>
2021-03-24 11:35:19 +0000

    Quantity Section worked on

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
+// Select a size function
+function selectSize() {
+  let select_size = document.getElementById("select-size");
+  let size = document.getElementById("size");
+  select_size.innerHTML = size.innerHTML;
+  size.style.backgroundColor = "#10005f";
+  size.style.color = "#ffffff";
+}
 
+// Decrease Quantity function
+function decreaseQuantity() {
+  var quantity = parseInt(document.getElementById("quantity").value);
+  quantity = isNaN(quantity) ? 0 : quantity;
+  quantity < 1 ? (quantity = 1) : "";
+  quantity--;
+  document.getElementById("quantity").value = quantity;
+}
+// Increase Quantity function
+function increaseQuantity() {
+  var quantity = parseInt(document.getElementById("quantity").value);
+  quantity = isNaN(quantity) ? 0 : quantity;
+  quantity++;
+  document.getElementById("quantity").value = quantity;
+}
